5f0c97cc7aa5c81f63237c902aa5319c030aef72
hiteshduttmathur <root@twentyeightjuly.asia-south1-c.c.crested-acumen-243111.internal>
2019-07-28 11:48:16 +0000

    new commit with combined comand

--- myapp.py
+++ myapp.py
@@ -1,10 +1,11 @@
 print("hello world)
-add some gibberish
+add  gibberish
 add more gibberis
 add new more lines here
 this is the forth line sirtesh dutt mathur..
 
 
-
+name ="hitesh"
+print(name)
 
 i have added 17:10 28 july 2019
