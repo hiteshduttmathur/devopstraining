19d3fd62a3a387fa48928f442d86836d40fe5b72
hiteshduttmathur <50262846+hiteshduttmathur@users.noreply.github.com>
2019-07-31 19:51:14 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,20 +1,4 @@
 print("hello world)
-add  gibberish
-add more gibberis
-add new more lines here
-this is the forth line sirtesh dutt mathur..
 
-
-name ="hitesh"
+      name ="hitesh"
 print(name)
-
-Jeevan ka Rahasys 
-
-
- will commit this into last line.
-
-
-
-Today is tuesday.
-
-This is done in dev branch.
